03af0bc42d8ff2ca48f9caccc0ce89b554849e21
Jesse Stuart <jesse@jessestuart.ca>
2015-02-08 14:10:31 -0500

    test: address PR comments
    Signed-off-by: Jesse Stuart <jesse@jessestuart.ca>

--- test/test_taffy_deploy.go
+++ test/test_taffy_deploy.go
@@ -14,31 +14,8 @@ type TaffyDeploySuite struct {
 
 var _ = c.Suite(&TaffyDeploySuite{})
 
-func (s *TaffyDeploySuite) TestDeploys(t *c.C) {
+func (s *TaffyDeploySuite) deployWithTaffy(t *c.C, app *ct.App, github map[string]string) {
 	client := s.controllerClient(t)
-
-	github := map[string]string{
-		"user_login": "flynn-examples",
-		"repo_name":  "go-flynn-example",
-		"ref":        "master",
-		"sha":        "a2ac6b059e1359d0e974636935fda8995de02b16",
-		"clone_url":  "https://github.com/flynn-examples/go-flynn-example.git",
-	}
-
-	// initial deploy
-
-	app := &ct.App{
-		Meta: map[string]string{
-			"type":       "github",
-			"user_login": github["user_login"],
-			"repo_name":  github["repo_name"],
-			"ref":        github["ref"],
-			"sha":        github["sha"],
-			"clone_url":  github["clone_url"],
-		},
-	}
-	t.Assert(client.CreateApp(app), c.IsNil)
-	debugf(t, "created app %s (%s)", app.Name, app.ID)
 
 	taffyRelease, err := client.GetAppRelease("taffy")
 	t.Assert(err, c.IsNil)
@@ -66,39 +43,25 @@ func (s *TaffyDeploySuite) TestDeploys(t *c.C) {
 	var outBuf bytes.Buffer
 	exit, err := attachClient.Receive(&outBuf, &outBuf)
 	t.Log(outBuf.String())
-	outBuf.Reset()
 	t.Assert(exit, c.Equals, 0)
 	t.Assert(err, c.IsNil)
+}
 
-	// second deploy
+// This test emulates deploys in the dashboard app
+func (s *TaffyDeploySuite) TestDeploys(t *c.C) {
+	client := s.controllerClient(t)
 
-	github["sha"] = "2bc7e016b1b4aae89396c898583763c5781e031a"
-
-	artifact := &ct.Artifact{Type: "docker", URI: imageURIs["test-apps"]}
-	t.Assert(client.CreateArtifact(artifact), c.IsNil)
-
-	release, err := client.GetAppRelease(app.ID)
-	t.Assert(err, c.IsNil)
-
-	release = &ct.Release{
-		ArtifactID: artifact.ID,
-		Env:        release.Env,
-		Processes:  release.Processes,
+	github := map[string]string{
+		"user_login": "flynn-examples",
+		"repo_name":  "go-flynn-example",
+		"ref":        "master",
+		"sha":        "a2ac6b059e1359d0e974636935fda8995de02b16",
+		"clone_url":  "https://github.com/flynn-examples/go-flynn-example.git",
 	}
-	t.Assert(client.CreateRelease(release), c.IsNil)
-	t.Assert(client.SetAppRelease(app.ID, release.ID), c.IsNil)
 
-	taffyRelease, err = client.GetAppRelease("taffy")
-	t.Assert(err, c.IsNil)
+	// initial deploy
 
-	rwc, err = client.RunJobAttached("taffy", &ct.NewJob{
-		ReleaseID: taffyRelease.ID,
-		Cmd: []string{
-			app.Name,
-			github["clone_url"],
-			github["ref"],
-			github["sha"],
-		},
+	app := &ct.App{
 		Meta: map[string]string{
 			"type":       "github",
 			"user_login": github["user_login"],
@@ -106,14 +69,41 @@ func (s *TaffyDeploySuite) TestDeploys(t *c.C) {
 			"ref":        github["ref"],
 			"sha":        github["sha"],
 			"clone_url":  github["clone_url"],
-			"app":        app.ID,
 		},
-	})
+	}
+	t.Assert(client.CreateApp(app), c.IsNil)
+	debugf(t, "created app %s (%s)", app.Name, app.ID)
+
+	s.deployWithTaffy(t, app, github)
+
+	_, err := client.GetAppRelease(app.ID)
 	t.Assert(err, c.IsNil)
 
-	attachClient = cluster.NewAttachClient(rwc)
-	exit, err = attachClient.Receive(&outBuf, &outBuf)
-	t.Log(outBuf.String())
-	t.Assert(exit, c.Equals, 0)
+	// second deploy
+
+	github["sha"] = "2bc7e016b1b4aae89396c898583763c5781e031a"
+
+	// create placeholder artifact
+	artifact := &ct.Artifact{Type: "docker", URI: "example://uri"}
+	t.Assert(client.CreateArtifact(artifact), c.IsNil)
+
+	release, err := client.GetAppRelease(app.ID)
 	t.Assert(err, c.IsNil)
+
+	release = &ct.Release{
+		ArtifactID: release.ArtifactID,
+		Env:        release.Env,
+		Processes:  release.Processes,
+	}
+	t.Assert(client.CreateRelease(release), c.IsNil)
+	t.Assert(client.SetAppRelease(app.ID, release.ID), c.IsNil)
+
+	s.deployWithTaffy(t, app, github)
+
+	newRelease, err := client.GetAppRelease(app.ID)
+	t.Assert(err, c.IsNil)
+	t.Assert(newRelease.ID, c.Not(c.Equals), release.ID)
+	release.Env["SLUG_URL"] = newRelease.Env["SLUG_URL"] // SLUG_URL will be different
+	t.Assert(release.Env, c.DeepEquals, newRelease.Env)
+	t.Assert(release.Processes, c.DeepEquals, newRelease.Processes)
 }
